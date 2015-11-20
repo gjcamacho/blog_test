848627a4f815efd73ada679f51e8270a428c95c4
Gerardo Camacho <gjcamacho@hotmail.com>
2015-11-19 23:45:50 -0600

    PostsController correction

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -1,7 +1,7 @@
 <?php
 App::uses('AppController', 'Controller');
 
-class DraftsController extends AppController{
+class PostsController extends AppController{
 	public function index(){
 		$this->set('posts',$this->Post->find('all'));
 	}
