f5216d07bf3d7dfcccf972123a087a3fac5cb1b9
Gerardo Camacho <gjcamacho@hotmail.com>
2015-12-04 18:39:25 -0600

    comment agregado

---
+++ app/Model/Comment.php
@@ -0,0 +1,12 @@
+<?php
+App::uses('AppModel', 'Model');
+
+class Comment extends AppModel {
+	public $belongsTo=array(
+			"Post"=>array(
+					"className"=>"Post",
+					"foreignKey"=>"post_id"
+			)
+	)
+}
+?>
--- app/Model/Post.php
+++ app/Model/Post.php
@@ -2,5 +2,10 @@
 App::uses('AppModel', 'Model');
 
 class Post extends AppModel {
+	public $hasMany=array(
+			"Comment"=>array(
+					"className"=>"Comment"
+			)
+	);
 }
 ?>
