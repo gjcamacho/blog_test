692611f9cf7457af50e26247bea07fadead634b2
Gerardo Camacho <gjcamacho@hotmail.com>
2015-12-04 18:24:11 -0600

    add() added to the Controller

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -5,5 +5,14 @@ class PostsController extends AppController{
 	public function index(){
 		$this->set('posts',$this->Post->find('all'));
 	}
+	
+	public function add(){
+		if($this->request->is("post")){
+			$this->Post->create();
+			$this->Post->save($this->request->data);
+			
+			$this->redirect(array("controller"=>"posts", "action"=>"index"));
+		}
+	}
 }
 ?>
