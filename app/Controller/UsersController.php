92a38b684238f6867d48f0c9870397a94babbd23
Gerardo Camacho <gjcamacho@hotmail.com>
2015-12-08 20:04:54 -0600

    se agrega variable de sesion

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -3,6 +3,16 @@ App::uses("AppController","Controller");
 
 class UsersController extends AppController{
 	public function login(){
+		if($this->request->is("post")){
+			$username=$this->request->data["User"]["username"];
+			$password=$this->request->data["User"]["password"];
+			
+			if($username=="gjcamacho" && $password="gjcamacho"){
+				$this->Session->write("User",array("User"=>array("username"=>"gjcamacho")));
+				
+				$this->redirect(array("controller"=>"posts", "action"=>"index"));
+			}
+		}
 		
 	}
 	
